090bee27de5451c91cd5cb2c7158030d24f2eddf
Adrian Ortmann <adrian.ortmann@online.de>
2013-07-24 19:45:05 +0200

    you can choose with your arguments port and branch

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
 var express = require('express');
 var app = express();
 
+var wanted_branch = process.argv[3];
+var port = process.argv[2];
 
-
-var wanted_branch = 'refs/heads/' + 'master';
-
-
-if(process.argv[2]){
-	var port = process.argv[2];
-} else {
-	port = 8001
-}
-
+console.log('Hook receiver is listening on port: ' +port+ ' and it\'s listening on branch: ' +wanted_branch);
 
 app.use(express.bodyParser());
 
 app.post('/recipes', function(request, response){
- 
+
+
 	var right_body = give_back_when_right_branch(wanted_branch, request.body['ref']);
+
+	console.log(right_body);
 
 	if(right_body) {
 		// git pull!!!
